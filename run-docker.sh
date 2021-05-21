c762cb233671ff7853d9bb1cbf265487ad41aed2
Puran Lai <1696095803@qq.com>
2021-05-21 20:23:29 +0800

    Update run-docker.sh

--- run-docker.sh
+++ run-docker.sh
@@ -1,4 +1,4 @@
 JEKYLL_VERSION=3.8
 
 docker run -d -p 4000:4000 --name blog_web --volume="$PWD:/srv/jekyll" jekyll/jekyll:$JEKYLL_VERSION   jekyll server --watch
-echo 'open brower vist http://localhost:4000'
+echo 'open browser vist http://localhost:4000'
